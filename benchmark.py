ce6bdc4e0402c2c20552a159dc03ea54c18f7aa8
Tokuhiro Matsuno <tokuhirom@gmail.com>
2016-11-23 11:40:12 +0900

    change directory structure

--- benchmark.py
+++ benchmark.py
@@ -2,36 +2,50 @@ import urllib2
 import subprocess
 import sys
 import os
+import errno
+
+def mkdir_p(path):
+	try:
+		os.makedirs(path)
+	except OSError as exc:  # Python >2.5
+		if exc.errno == errno.EEXIST and os.path.isdir(path):
+			pass
+		else:
+			raise
 
 class Benchmarker:
-	def __init__(self, host, port, port2name):
-		self.host = host
+	def __init__(self, ip, port, port2impl, ip2instance_type):
+		self.ip = ip
+		self.instance_type = ip2instance_type[ip]
 		self.port = port
-		self.port2name = port2name
+		self.impl = port2impl[port]
 
 	def mk_cmdline(self, connections):
 		return ['wrk', '--timeout', '10s', '--latency', '-t', '4', '-c', str(connections), self.url()]
 
 	def url(self):
-		return "http://%s:%s/" % (self.host, self.port)
+		return "http://%s:%s/" % (self.ip, self.port)
 
 	def run(self):
 		print "======> WARMING UP <======="
 		subprocess.check_call(self.mk_cmdline(10))
 
+		basedir = 'result/' + self.instance_type
+		mkdir_p(basedir)
+
 		print "======> RUN <======="
 		for connections in [10, 100, 1000, 10000]:
 			print "Connections: %d" % connections
-			fname = "result/%s-%d.txt" % (self.port2name[port], connections)
+			fname = "%s/%s-%d.txt" % (basedir, self.impl, connections)
 			with open(fname, 'w') as fh:
 				subprocess.check_call(self.mk_cmdline(connections), stdout=fh, stderr=fh)
 			with open(fname, 'r') as fh:
 				print fh.read()
 
-host = sys.argv[1]
+ip = sys.argv[1]
 port = sys.argv[2]
 
-benchmarker = Benchmarker(host, port, {
+benchmarker = Benchmarker(ip, port, {
 	"8080": "vertx",
 	"8081": "spring-boot-tomcat",
 	"8082": "spring-boot-jetty",
@@ -39,6 +53,8 @@ benchmarker = Benchmarker(host, port, {
 	"8084": "undertow",
 	"8085": "jetty",
 	"8086": "tomcat"
+}, {
+	'163.172.184.153': 'vps'
 })
 benchmarker.run()
 
